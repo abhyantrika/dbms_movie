a7e666cd0e295c95597a3649a5b3edcf49ae5ac6
TilakSN <TilakSN@users.noreply.github.com>
2016-12-01 20:39:57 +0530

    Add files via upload
    sign.html alone implements both sign in and sign up
    added bootstrap to index and sign
    fixed offline features in app and get_movie

--- app.py
+++ app.py
@@ -1,122 +1,149 @@
-from flask import Flask,flash,render_template, json, request,redirect,url_for,jsonify
+from flask import Flask, flash, render_template, request, redirect
 from werkzeug import generate_password_hash, check_password_hash
 import MySQLdb as my 
-import json,yaml
-from get_movie import*
+import yaml
+from get_movie import *
 
 """This File uses MySQL ONLY"""
 
 app = Flask(__name__)
-db = my.connect("localhost","root","themenwhopause","login" )
-app.secret_key='123456'
+db = my.connect(ADDRESS, USERNAME, PASSWORD, "login")
+app.secret_key = KEY
+home_address = "http://%s:%d" % (ADDRESS, PORT)
+USN = ''
+EMAIL = ''
 
-#___________________________________________________#
-"""Stupid Home Page """
+# ___________________________________________________ #
+"""Home Page """
+
 
 @app.route('/')
 def home():
-	return render_template('index.html')
-#____________________________________________________#
+    print "home"
+    return render_template('index.html', addr=home_address)
+# ___________________________________________________ #
 """Sign up and Sign in functions"""
 
-@app.route('/signup_help')
+
+@app.route('/signup')
 def signup_help():
-	return render_template('form.html')
+    global USN, EMAIL
+    email = EMAIL
+    USN = EMAIL = ''
+    print "signup", USN, EMAIL
+    return render_template('sign.html', type=1, email=email, username='')
 
-@app.route('/signin_help')
+
+@app.route('/signin')
 def signin_help():
-	return render_template('signin_form.html')
+    global USN, EMAIL
+    username = USN
+    USN = EMAIL = ''
+    print "signin", USN, EMAIL
+    return render_template('sign.html', type=2, email='', username=username)
 
-@app.route('/main',methods = ['POST','GET'])
+
+@app.route('/main', methods=['POST', 'GET'])
 def sign_up_in():
-	if request.method == 'POST':
-		result = request.form 
-		#print result
-	try:
-		print result['email']				# If email present : it means it is sign up, else sign in!
-		with open('signup.json','w') as f:
-			f.write(json.dumps(result))		#Temporarily write to json, the db funcs will use this to insert or validate.
-		update_db()
-		return render_template('main.html')	
-	except:
-		with open('signin.json','w') as f:
-			f.write(json.dumps(result))
+    global EMAIL, USN
+    print "main"
+    if request.method == 'POST':
+        result = request.form
+        print "Result:", result
+    if result['email']:                           # If email present : it means it is sign up, else sign in!
+        dump_to_json('signup.json', result)         # Temporarily write to json, the db funcs will use this to insert or validate.
+        if not update_db():
+            EMAIL = result['email']
+            return redirect('/signup')
+        return redirect('/movie')    
+    else:
+        dump_to_json('signin.json', result)
+        if check_with_db():
+            return redirect('/movie')
+        else:
+            flash("Please Try Again")
+            USN = result['username']
+            return redirect('/signin')
 
-		if check_with_db():
-			return render_template('main.html')
-		else:
-			flash("Please Try Again")
-			return render_template('login_fail.html')
 
-#___________________________________________________________#
+@app.route('/movie')
+def movie():
+    print "movie"
+    return render_template('movie.html')
+
+# ___________________________________________________________ #
 """Results from api: Use get_movie.py"""
 
-@app.route('/results',methods = ['POST','GET'])
+
+@app.route('/results', methods=['POST', 'GET'])
 def results():
-	if request.method == 'POST':	
-		result = request.form 
-		#print result
-	try:
-		movie_info = get_movie(result['Title'].encode('utf-8'))
-		#print movie_info
-		return render_template('results.html',dict = movie_info)
-	except:
-		flash("Invalid, try again!")
+    print "results"
+    if request.method == 'POST':    
+        result = request.form
+    try:
+        movie_info = get_movie(result['Title'].encode('utf-8'))
+        return render_template('results.html', dict=movie_info)
+    except:
+        flash("Invalid, try again!")
+        return redirect('/movie')
+
 
 @app.route('/senti')
 def senti():
-	f = open('movie.json','r').read()
-	parsed_json = yaml.safe_load(f)
-	senti_dict = senti_analysis(parsed_json['Title'])
-	#print senti_dict
-	return render_template('senti.html',list =senti_dict['document_tone']['tone_categories'])
+    f = open('movie.json', 'r').read()
+    parsed_json = yaml.safe_load(f)
+    senti_dict = senti_analysis(parsed_json['Title'])
+    return render_template('senti.html', list=senti_dict['document_tone']['tone_categories'])
 
-#______________________________________________________#
+# ______________________________________________________ #
+
+
 @app.route('/logout')
 def logout():
-	"""
-	#Try Later if time permits
-	query = "delete UserName from logged_users where UserName ..."
-	"""
-	pass
-	
-#_______________________________________________________#
-
+    """
+    # Try Later if time permits
+    query = "delete UserName from logged_users where UserName ..."
+    """
+    dump_to_json("signin.json", {})
+    dump_to_json("signup.json", {})
+    return home()
+    
+# _______________________________________________________ #
 """Database stuff"""
 
+
 def check_with_db():
-	f = open('signin.json').read() 
-	f = json.loads(f)
-	password = f['password']
-	sql = "Select password from user where UserName = \'" + f['username'] + "\';"
-	try:
-		cursor = db.cursor()
-		cursor.execute(sql)
-		db.commit()
-		data = list(cursor.fetchone()) + [password]
-		#print data 
-		return check_password_hash(data[0],data[1])
-	except:
-		db.rollback()
-		return False
+    f = open('signin.json').read() 
+    f = json.loads(f)
+    password = f['password']
+    sql = "Select password from user where userName = '" + f['username'] + "';"
+    try:
+        cursor = db.cursor()
+        cursor.execute(sql)
+        db.commit()
+        data = list(cursor.fetchone()) + [password]
+        return check_password_hash(data[0], data[1])
+    except:
+        db.rollback()
+        return False
+
 
 def update_db():
-	f = open('signup.json').read() 
-	f = json.loads(f)
-	password = generate_password_hash(f['password'])
-	try:
-		sql = "insert into user values(\'" + f['username'] + "\',\'" + f['email'] + "\' ,\'"+ password +'\')'+';'
-		print sql
-		cursor = db.cursor()
-		cursor.execute(sql)
-		db.commit()
-		print'Success-> commit'
-	except:
-		print"Nope rollback!!"
-		db.rollback()
+    f = open('signup.json').read()
+    f = json.loads(f)
+    password = generate_password_hash(f['password'])
+    try:
+        sql = "insert into user values('" + f['username'] + "','" + f['email'] + "' ,'"+ password +"');"
+        cursor = db.cursor()
+        cursor.execute(sql)
+        db.commit()
+    except:
+        db.rollback()
+        return False
+    return True
 
-#_______________________________________________________#
+# _______________________________________________________ #
 
 if __name__ == "__main__":
-	app.run(port=5000)
+    app.run(debug=True, port=PORT)
 
--- get_movie.py
+++ get_movie.py
@@ -1,55 +1,62 @@
-import json,urllib2
+import json
+import urllib2
 import pymongo
 import yaml
+from watson_developer_cloud import ToneAnalyzerV3
+from configuration import USERNAME, PASSWORD, KEY, PORT, ADDRESS
+from watson_api import WATSON
 
-conn = pymongo.MongoClient('localhost')              #Mongo Stuff
-conn.admin.authenticate('root','themenwhopause')
+conn = pymongo.MongoClient(ADDRESS)              # Mongo Stuff
+conn.admin.authenticate(USERNAME, PASSWORD)
 db = conn.admin
 rec = db.movie
+tone_analyzer = None
+
+
+def dump_to_json(file_name, object_name):
+    with open(file_name, "w") as f:
+        f.write(json.dumps(object_name))
+
+
+def get_senti():
+    global tone_analyzer
+    if tone_analyzer is None:
+        try:
+            tone_analyzer = ToneAnalyzerV3(**WATSON)
+        except:
+            print "Watson's out of coverage"
+    return tone_analyzer
+
 
 def get_movie(movie_name):
-	movie_name = movie_name.replace(' ','+')
-	print "movie_name",movie_name
-	api_end = "http://www.omdbapi.com/?t=%s&y=&plot=short&r=json" %(movie_name)
-	
-	movie_in_db = rec.find_one({"Title":{'$regex':movie_name,'$options':"$i"}})
-	print movie_in_db
+    api_end = "http://www.omdbapi.com/?t=%s&y=&plot=short&r=json" % movie_name.replace(' ','+')
+    page = rec.find_one({'Title': {'$regex': movie_name, '$options': "$i"}})
+    found = page is not None
+    if found:
+        del page['_id']
+    else:
+        try:
+            page = eval(urllib2.urlopen(api_end).read())
+        except:
+            return {}
+    dump_to_json('movie.json', page)
+    if not found:
+        insert_to_mongo()
+    return page
 
-	if(movie_in_db != None):
-		print "Movie found in mongo!"			#First search for entry in MOngo, if not found, go online!
-		return movie_in_db
-	else:		
-		print "Movie not in mongo, fetching..."
-		try:
-			page = eval(urllib2.urlopen(api_end).read())
-			print "Page found"
-		except:
-			print "Error 404"
-		with open('movie.json','w') as f:
-			f.write(json.dumps(page))	
-		return insert_to_mongo(movie_name)
 
 def senti_analysis(movie_name):
-	from watson_developer_cloud import ToneAnalyzerV3
-	tone_analyzer = ToneAnalyzerV3(
-	username='ba6cd88a-0af9-4a2d-b9e7-f0a4ff8c50c0',
-	password='0cKi5z7W73sW',
-	version='2016-05-19')
-	movie_in_db = rec.find_one({"Title":{'$regex':movie_name,'$options':"$i"}})
-	return tone_analyzer.tone(text=movie_in_db['Plot'])
-
-def insert_to_mongo(movie_name):
-	f = open('movie.json','r').read()		#All json files used are temporary!	
-	parsed_json = yaml.safe_load(f)
-	try:
-		rec.insert(parsed_json)
-		print 'Inserted into mongo!'
-		return parsed_json
-	except:
-		print"Can't insert into mongo"	
+    movie_in_db = rec.find_one({"Title": {'$regex': movie_name, '$options': "$i"}})
+    analyzer = get_senti()
+    if analyzer is None:
+        return {}
+    return analyzer.tone(text=movie_in_db['Plot'])
 
 
-
-
-
-
+def insert_to_mongo():
+    f = open('movie.json', 'r').read()        # All json files used are temporary!
+    parsed_json = yaml.safe_load(f)
+    try:
+        rec.insert(parsed_json)
+    except:
+        pass
